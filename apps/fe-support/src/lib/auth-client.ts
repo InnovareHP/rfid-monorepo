a573c345eca8ec17c4de52596aae10b78ecc4dd6
redcloud442 <cloudred442@gmail.com>
2026-02-13 15:30:48 +0800

    add deployment

--- apps/fe-support/src/lib/auth-client.ts
+++ apps/fe-support/src/lib/auth-client.ts
@@ -1,9 +1,8 @@
-import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_API_URL,
-  plugins: [adminClient()],
+  plugins: [],
 });
 
 export const { signIn, signUp, signOut, useSession, refreshToken } = authClient;
