9762ae7d65d7abe4946ea46d8d58c2840318cc86
redcloud442 <cloudred442@gmail.com>
2026-02-27 15:43:08 +0800

    fix cookie now

--- apps/fe-support/src/lib/auth-client.ts
+++ apps/fe-support/src/lib/auth-client.ts
@@ -2,15 +2,8 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL,
   plugins: [adminClient()],
 });
 
-export const {
-  signIn,
-  signUp,
-  signOut,
-  useSession,
-  refreshToken,
-  getSession,
-} = authClient;
+export const { signIn, signUp, signOut, useSession, refreshToken, getSession } =
+  authClient;
